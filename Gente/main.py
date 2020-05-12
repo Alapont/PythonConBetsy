728b15d99dd8e8c5426418b879e1bf62648ce5b4
Alapont <pablodragon@gmail.com>
2020-05-12 19:05:21 +0200

    unimos colores y nombres

--- Gente/main.py
+++ Gente/main.py
@@ -1,17 +1,25 @@
 import persona
 #Usamos personas
 
+nombres = ["Pepe", "Juan", "Dieter", "Kinþaswinþs","Бладимир Путин","Obama", "TechnoViking","Betsy"]
 lista = []
-nombres = ["Pepe", "Juan", "Dieter", "Kinþaswinþs"]
+hain=[]
+
+def escupir (persona):
+    print("Que te jodan "+persona.nombre)
 
 for nombre in nombres :
     print(nombre)
     lista.append ( persona.Persona (nombre) )
 
-pass
 
 for p in lista :
-    p.presebtate()
-    
+    p.presentate()
+    if (p.colorDeCalcetines != "blanco") :
+        hain.append(p)
+    else:
+        escupir(p)
+
+# En Hain solo hay gente sin calcetines blancos
 
 pass
--- Gente/persona.py
+++ Gente/persona.py
@@ -1,12 +1,21 @@
 # Persona
+import random
+from string import Template
+
+
 
 class Persona:
     # Constructor por parametros
-    def __init__(self, nombre, colorDeCalcetines):
+    def __init__(self, nombre):
         self.nombre=nombre
-        self.colorDeCalcetines=colorDeCalcetines
+        self.colorDeCalcetines= random.choice(["blanco","magenta","furcia"])
 
     def presentate (self):
-        print ("Hola, me llamo: "+self.nombre)
+        presentacion=Template("Holi, me llamo: $nombre y tengo los calcetines de color $calcetines")
+
+        print (presentacion.substitute(
+            nombre=self.nombre,
+            calcetines=self.colorDeCalcetines
+        ))
         
 
