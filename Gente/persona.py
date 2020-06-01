0c663e03e8aadf85e88a85aa4c932ac8b00f2fe4
Alapont <pablodragon@gmail.com>
2020-06-01 19:28:53 +0200

    empezamos a emborracharnos

--- Gente/persona.py
+++ Gente/persona.py
@@ -2,6 +2,7 @@
 import random
 from string import Template
 
+MAX_HIGADO=100
 
 class Persona:
     # Constructor por parametros
@@ -9,21 +10,36 @@ class Persona:
         self.nombre = nombre
         self.presentacion = getPresentacion(idioma)
         self.colorDeCalcetines = random.choice(["blanco", "magenta", "furcia"])
+        self.dinero=100
+        self.higado=0
 
     def presentate(self):
         print(self.presentacion.substitute(
             nombre=self.nombre,
             calcetines=self.colorDeCalcetines
         ))
-    # ToDo: emborracharse
+    
+    def beber(self, bebida, coste):
+        if( coste < self.dinero ):
+            self.dinero = self.dinero - coste
+            self.higado = self.higado + bebida
+            if self.higado >= MAX_HIGADO:
+                self.potar()
+        else: #no bebo
+            pass
+
+    def potar(self):
+        print(self.name+"🤮")
 
 def getPresentacion(idioma):        
     if (idioma=="es-es"):
         return Template(
-            "Holi, me llamo: $nombre y tengo los calcetines de color $calcetines")
+            "Holi, me llamo: $nombre y tengo los calcetines de color $calcetines"
+        )
     elif (idioma=="de"):
         return Template(
-        "Hallo, ich bin $nombre und ich hasse $calcetines Socken")
+            "Hallo, ich bin $nombre und ich hasse $calcetines Socken"
+        )
     elif (idioma=="fr"):
         return Template(
             "Bonjour, Je m'apelle $nombre, et j'ai des chausettes $calcetines"
@@ -33,5 +49,10 @@ def getPresentacion(idioma):
             "Здравствуйте, меня зовут $nombre и у меня красные $color"
         )
     else: # valor por defecto
-        self.presentacion = Template(
-        "🤫")
+        return Template(
+            "🤫"
+        )
+
+aux = Persona()
+aux.beber(10,0)
+print("fin de pruebas")
