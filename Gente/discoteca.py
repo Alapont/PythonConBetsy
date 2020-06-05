89636b224995c693a5a5fd1859a1b3cf25ac8e53
Alapont <pablodragon@gmail.com>
2020-06-05 19:16:34 +0200

    hacemos discotecas

---
+++ Gente/discoteca.py
@@ -0,0 +1,27 @@
+# Discoteca unts unts
+
+class Discoteca:
+    # Constructor por parametros
+    def __init__(self, nombre="club momentos",aforoMaximo=100):
+        self.nombre = nombre
+        self.aforoMaximo=aforoMaximo
+        self.dinero=0
+        self.colaEntrada=[]
+        self.genteDentro=[]
+
+    def añadirColaEntrar(self, persona):
+        self.colaEntrada.append(persona)
+
+    def intentarPasar (self):
+        if ( len (self.genteDentro) < self.aforoMaximo):
+            siguiente=self.colaEntrada.pop(0)
+            if (siguiente.colorDeCalcetines != "blanco"):
+                self.genteDentro.append(siguiente)
+            else :
+                # print("heute leider nicht +siguiente.nombre)
+                print("Que te jodan "+siguiente.nombre)
+        else:
+            print("no hay mas hueco")
+
+
+print("fin de pruebas")
